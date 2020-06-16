392bbf9deb530df1ee266de1cc71bff95ebb258f
Suraj Bastola <thebananacider@hotmail.com>
2020-06-16 14:05:25 +0545

    ejs

--- api/src/server.js
+++ api/src/server.js
@@ -1,18 +1,28 @@
-const express = require ("express")
+const express = require("express")
 const bodyParser = require("body-parser")
 const {APP_PORT} = require("./config")
 const {logger} = require("./middlewares/")
 const app = express()
 app.use(bodyParser.json());
 app.use(logger);
+/*
+Required by EJS
+ */
+app.use(express.static(__dirname + '/assets'))
+app.set('views', __dirname + '/views')
+app.engine('html', require('ejs').renderFile)
+app.set('view engine', 'html')
+/*
+Required by EJS
+ */
 require("./routes")(app)
-app.get("/",(req, res)=>{
+app.get("/", (req, res) => {
     // console.log("Appid =",req.appId)
-    res.send("Welcome to the Backend with MERN")
+    res.render('index')
 })
-app.get("/*/*",(req, res)=>{
+app.get("/*/*", (req, res) => {
     res.status(404).send("Page Not Found")
 })
-app.listen(APP_PORT,()=>{
+app.listen(APP_PORT, () => {
     console.log(`App is Running on http://localhost:${APP_PORT}`)
 })
