d5c713bed1c1250deb2535d5d56b2c41a36257c5
Suraj Bastola <thebananacider@hotmail.com>
2020-06-21 22:04:58 +0545

    Header Generation

---
+++ api/src/middlewares/auth.js
@@ -0,0 +1,13 @@
+const {verify} = require('jsonwebtoken')
+const {JW_SECRET_ACCESS_TOKEN} = require('../config')
+exports.auth = (req, res, next) => {
+    const authorization = req.headers.authorization
+    if (!authorization) {
+        throw new Error("Unauthorized Access")
+    }
+    const accessToken = authorization.split(" ")[1]
+    const payload = verify(accessToken, JW_SECRET_ACCESS_TOKEN)
+    console.log("----", payload)
+
+    next()
+}
--- api/src/middlewares/index.js
+++ api/src/middlewares/index.js
@@ -1,4 +1,5 @@
 const {logger} = require("./logger")
+const {auth} = require("./auth")
 module.exports = {
-    logger,
+    logger, auth,
 }
